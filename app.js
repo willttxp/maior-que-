f3f46b862522e4244f5dfa043987087337b8d34a
willttxp <willimvitolimaamorim13@gmail.com>
2022-03-07 18:55:48 -0300

    codigo completo

---
+++ app.js
@@ -0,0 +1,7 @@
+var ida= prompt("qual sua idade ?")
+if( ida >= 18){ alert ("voce é um jovem")}
+else if( ida <= 17){alert("voce é um bb kkk")}
+if ( ida > 60){alert("voce é um veio")}
+else { }
+
+
